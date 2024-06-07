6ae00a7c984d3acaca805509b59f2c431e6062e3
srvkmr699 <srvkmr699@gmail.com>
2024-06-07 14:14:05 +0530

    added jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git 'https://github.com/srvkmr699/performance-test.git'
+            }
+        }
+        stage('Run JMeter Tests') {
+            steps {
+                script {
+                    docker.image('jmeter:latest').inside {
+                        sh '''
+                            jmeter -n -t /jmeter/test.jmx -l /jmeter/results/results.jtl \
+                            -Jjmeter.save.saveservice.output_format=xml \
+                            -e -o /jmeter/results/report
+                        '''
+                    }
+                }
+            }
+        }
+        stage('Publish Results') {
+            steps {
+                perfReport filterRegex: 'jmeter/results/results.jtl'
+            }
+        }
+        stage('Send Data to InfluxDB') {
+            steps {
+                script {
+                    docker.image('influxdb:latest').inside {
+                        sh '''
+                            influx -host influxdb -username admin -password admin123 \
+                            -database jmeter -execute 'import /jmeter/results/results.jtl'
+                        '''
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'jmeter/results/results.jtl'
+        }
+    }
+}
